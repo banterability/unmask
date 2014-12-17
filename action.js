49c848bf8a47d3d86f6a953000d36da70cb11b3c
Jeffrey Long <jlong@groupon.com>
2014-12-17 13:16:15 -0600

    Flash fields being unmasked

--- action.js
+++ action.js
@@ -1,6 +1,7 @@
 var passwordFields = document.querySelectorAll('input[type="password"]');
 
 [].forEach.call(passwordFields, function(node){
+  node.classList.add('unmask-field');
   node.type='text';
 });
 
--- background.js
+++ background.js
@@ -1,4 +1,7 @@
 chrome.browserAction.onClicked.addListener(function(tab){
+  chrome.tabs.insertCSS({
+    file: 'styles.css'
+  });
   chrome.tabs.executeScript({
     file: 'action.js'
   });
