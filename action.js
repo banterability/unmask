15d26da982f8634bdd9cabfab7870fd9fb0cd41a
Jeffrey Long <jlong@groupon.com>
2014-12-19 14:33:54 -0600

    Allow unmasked fields to be toggled back

--- action.js
+++ action.js
@@ -1,6 +1,29 @@
-var passwordFields = document.querySelectorAll('input[type="password"]');
+function init(){
+  var passwordFields = document.querySelectorAll('input[type="password"]');
+  [].forEach.call(passwordFields, function(node){
+    node.dataset.unmask = true;
+  });
+  window['unmask-ext-fired'] = true;
+}
 
-[].forEach.call(passwordFields, function(node){
-  node.classList.add('unmask-field');
-  node.type='text';
-});
+function toggleFields(){
+  var passwordFields = document.querySelectorAll('[data-unmask]');
+  [].forEach.call(passwordFields, function(node){
+    if(node.className.indexOf('unmask-field') !== -1){
+      node.classList.remove('unmask-field');
+      node.type = 'password';
+    } else {
+      node.classList.add('unmask-field');
+      node.type = 'text';
+    }
+  });
+}
+
+var initialized = window['unmask-ext-fired'] === true;
+
+if(initialized){
+  toggleFields();
+}else{
+  init();
+  toggleFields();
+}
